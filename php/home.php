fadf19f5ab3d66e52c6272270c579ad5cf20ef98
bianca-pinto <71729898+Bianca-Pinto@users.noreply.github.com>
2023-10-30 18:17:30 -0300

    Add files via upload

---
+++ php/home.php
@@ -0,0 +1,8 @@
+<div class="row">
+    <center><h1>Bem-vindo ao painel de administrador!</h1>
+    <div class="col-lg-12">
+        <p>Cursos: 
+            <a href="../php/admin.php" class="btn btn-primary">Cadastrar usuário</a> 
+    </div>
+    </center>
+</div>
