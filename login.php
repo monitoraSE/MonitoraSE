fa6f6ce1cc56f717692039a8532ea7bfc6002666
bianca-pinto <71729898+Bianca-Pinto@users.noreply.github.com>
2023-11-21 07:36:52 -0300

    Add files via upload

--- login.php
+++ login.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MonitoraSE</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link href="styles/style.css" rel="stylesheet">
+    <link href="./styles/style.css" rel="stylesheet">
 </head>
 <body class="body__login">
     <?php
@@ -29,7 +29,7 @@
                     <label for="senha" class="email-senha">Senha</label>
                     <input type="password" id="senha" name="senha" class="input" placeholder="****************">
                 </div>
-                <div class=botao_login_enviar">
+                <div class="botao_login_enviar">
                     <input type="submit" value="Entrar" class="input-entrar">
                 </div>
 
